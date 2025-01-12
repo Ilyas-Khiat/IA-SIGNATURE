9703bcce76751a918656dfe2b871a1d8865c5520
Ilyas KHIAT <kiatdotblend@gmail.com>
2025-01-12 14:05:23 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-// src/App.tsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from "./components/LandingPage";
-import Chat from "./components/Chat";
 import Navigation from "./components/Navigation";
+import Chat from "./components/Chat";
 import ArtGalleryWithPopup from "./components/ArtGalleryWithPopup";
 import ArtGalleryTristan from "./components/ArtGalleryTristan";
 import WhatIf from "./components/whatif";
-import KnowledgeGraph from "./components/KnowledgeGraph"; // Import the new component
-import { Analytics } from '@vercel/analytics/next';
+import KnowledgeGraph from "./components/KnowledgeGraph";
+import { Analytics } from '@vercel/analytics/react'; // Changed from next to react
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      {/* Routes without navigation */}
-      <Route path="/" element={<LandingPage />} />
+    <div>
+      <Routes>
+        {/* Routes without navigation */}
+        <Route path="/" element={<LandingPage />} />
 
-      {/* Routes with navigation */}
-      <Route element={<Navigation />}>
-        <Route path="/conversation" element={<Chat />} />
-        <Route path="/magritte" element={<ArtGalleryWithPopup />} />
-        <Route path="/tristan" element={<ArtGalleryTristan />} />
-        <Route path="/whatif" element={<WhatIf />} />
-        <Route path="/knowledge_graph" element={<KnowledgeGraph />} /> {/* New Route */}
-        {/* Add other routes that should include the navigation here */}
-      </Route>
-    </Routes>
-    
+        {/* Routes with navigation */}
+        <Route element={<Navigation />}>
+          <Route path="/conversation" element={<Chat />} />
+          <Route path="/magritte" element={<ArtGalleryWithPopup />} />
+          <Route path="/tristan" element={<ArtGalleryTristan />} />
+          <Route path="/whatif" element={<WhatIf />} />
+          <Route path="/knowledge_graph" element={<KnowledgeGraph />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </div>
   );
 };
 
